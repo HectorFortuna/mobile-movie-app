6ead87faa27aea99da30af279eee73aeef6eae7c
Tito Fortuna <hectorsfortuna@gmail.com>
2025-08-04 14:33:26 -0300

    ci

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,24 @@
 // .eslintrc.js
-const { defineConfig } = require('eslint/config');
+const {defineConfig} = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
 
 module.exports = defineConfig([
-  expoConfig,
-  {
-    ignores: ['dist/*'],
-    settings: {
-      'import/resolver': {
-        node: {
-          extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.png'],
+    expoConfig,
+    {
+        ignores: ['dist/*'],
+        settings: {
+            'import/resolver': {
+                node: {
+                    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.png'],
+                },
+            },
         },
-      },
+
+        rules: {
+            '@typescript-eslint/no-unused-vars': 'warn',
+            'react-hooks/exhaustive-deps': 'warn',
+            'import/no-unresolved': 'off',
+        }
+
     },
-    rules: {
-      rules: {
-        '@typescript-eslint/no-unused-vars': 'warn',
-        'react-hooks/exhaustive-deps': 'warn',
-        'import/no-unresolved': 'off',
-      }
-    },
-  },
 ]);
